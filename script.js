42044699f67cb5a4bca34c4a1d800b61b860da30
Durga Waghchaure <durga.waghchaure.cc@gmail.com>
2026-06-06 12:25:18 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,9 +1,30 @@
+//variable and datatype
+let name = "Durga";      // String
+let age = 18;            // Number
+let isStudent = true;    // Boolean
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Student:", isStudent);
+
+//conditional statements
+let num = 10;
+
+if (num > 0) {
+    console.log("Positive Number");
+} else {
+    console.log("Negative Number");
+}
+
+
+
 // arithmatic operators
 let a=10
 let b=20
 console.log(a+b)
-console.log(a-b)console.log(a*b)
+console.log(a-b)
+console.log(a*b)
 console.log(a/b)
 console.log(a%b)
 console.log(a**b)
-//loops
+
