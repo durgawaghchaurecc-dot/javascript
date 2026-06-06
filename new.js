a5149bd8c8f448d61a7b9b645a06625307685f36
Durga Waghchaure <durga.waghchaure.cc@gmail.com>
2026-06-06 12:17:31 +0530

    initial commit

--- new.js
+++ new.js
@@ -1,9 +1,11 @@
+//for loop
 for (let i = 1; i <= 5; i++) {
     console.log(i);
 }
 
 let j = 1;
 
+//while loop
 while (j <= 5) {
     console.log(j);
     j++;
@@ -11,6 +13,7 @@ while (j <= 5) {
 
 let x = 1;
 
+//do while loop
 do {
     console.log(x);
     x++;
